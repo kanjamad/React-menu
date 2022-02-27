8f9d9e33a4a00773714fb1fab85a209007da14cf
Kanjamad Bosel <npor25@yahoo.com>
2022-02-26 22:38:36 -0800

    adding file for github pages

---
+++ build/precache-manifest.bb2651d33212190ab4e1e782e2b2647b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dcfa2a2adb25c43264564379c18fda6",
+    "url": "/React-menu/index.html"
+  },
+  {
+    "revision": "3da14468ad397ba8e4d7",
+    "url": "/React-menu/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "317cceaa682868a51da0",
+    "url": "/React-menu/static/js/2.cd2a891f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-menu/static/js/2.cd2a891f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da14468ad397ba8e4d7",
+    "url": "/React-menu/static/js/main.6be17861.chunk.js"
+  },
+  {
+    "revision": "887d3826b920d83214d8",
+    "url": "/React-menu/static/js/runtime-main.9abe60f0.js"
+  }
+]);
